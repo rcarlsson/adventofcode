47787dc287674a9c1e2bcfaac7c31f17ea309ca6
Robin Carlsson <robin9003@gmail.com>
2019-12-04 15:53:33 +0100

    Update init script
    Fetch input from aoc webpage

--- 2019/init_day.py
+++ 2019/init_day.py
@@ -1,12 +1,31 @@
 import sys
 import os
 from shutil import copyfile
+import requests
 
 day = sys.argv[1]
 
-if (os.path.isdir(day)):
-    print("Folder already exists, do nothing.")
-    sys.exit(0)
+# Create folder if it does not exist
+if (not os.path.isdir(day)):
+    os.mkdir(day)
 
-os.mkdir(day)
-copyfile("template.py", day+"/"+day+".py")
+# Copy template if script does not exist
+pyfile = day+'/'+day+'.py'
+if (not os.path.exists(pyfile)):
+    copyfile('template.py', pyfile)
+
+# Fetch input if input file does not exist
+inputfile = day+'/input'
+if (not os.path.exists(inputfile)):
+    cookie = open("../aoc_cookie").read().rstrip()
+    url = 'https://adventofcode.com/2019/day/'
+    if day[0] == '0':
+        url += day[1]
+    else:
+        url += day
+    url += '/input'
+
+    r = requests.get(url, cookies=dict({'session': cookie}))
+    f = open(inputfile, 'w+')
+    f.write(r.content)
+    f.close()
