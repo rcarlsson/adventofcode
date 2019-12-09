f6ee620e44df32aa36587ae5d0b99fd4bf415d11
Robin Carlsson <robin9003@gmail.com>
2019-12-09 14:24:29 +0100

    Cleanup parsing of file_name

--- 2019/05/05.py
+++ 2019/05/05.py
@@ -4,14 +4,10 @@ import os
 sys.path.append('../')
 import intcode
 
-file_name = ''
+file_name = os.path.realpath(__file__).rsplit('/',1)[0]+'/input'
 
 if len(sys.argv) > 1:
     file_name = sys.argv[1]
-else:
-    # If no file provided, try to find 'input' in script folder
-    path = os.path.realpath(__file__).rsplit('/',1)[0]
-    file_name = path+'/input'
 
 init_prog = [int(x) for x in open(file_name).read().split(',')]
 
